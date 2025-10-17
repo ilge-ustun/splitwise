f6123844b9053c0c9756b75ad3b238bb56db5fa4
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-10-17 22:11:40 +0200

    wip

--- frontend/src/config/wagmiNetworks.ts
+++ frontend/src/config/wagmiNetworks.ts
@@ -1,31 +1,29 @@
 import {
-  type AppKitNetwork,
   arbitrumSepolia,
-  arbitrum,
 } from '@reown/appkit/networks';
 
-export { arbitrumSepolia, arbitrum } from '@reown/appkit/networks';
+export { arbitrumSepolia } from '@reown/appkit/networks';
 
-export const bellecour: AppKitNetwork = {
-  id: 0x86,
-  name: 'iExec Sidechain',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'xRLC',
-    symbol: 'xRLC',
-  },
-  rpcUrls: {
-    public: { http: ['https://bellecour.iex.ec'] },
-    default: { http: ['https://bellecour.iex.ec'] },
-  },
-  blockExplorers: {
-    etherscan: {
-      name: 'Blockscout',
-      url: 'https://blockscout-bellecour.iex.ec',
-    },
-    default: { name: 'Blockscout', url: 'https://blockscout-bellecour.iex.ec' },
-  },
-};
+// export const bellecour: AppKitNetwork = {
+//   id: 0x86,
+//   name: 'iExec Sidechain',
+//   nativeCurrency: {
+//     decimals: 18,
+//     name: 'xRLC',
+//     symbol: 'xRLC',
+//   },
+//   rpcUrls: {
+//     public: { http: ['https://bellecour.iex.ec'] },
+//     default: { http: ['https://bellecour.iex.ec'] },
+//   },
+//   blockExplorers: {
+//     etherscan: {
+//       name: 'Blockscout',
+//       url: 'https://blockscout-bellecour.iex.ec',
+//     },
+//     default: { name: 'Blockscout', url: 'https://blockscout-bellecour.iex.ec' },
+//   },
+// };
 
 // Explorer slugs mapping for iExec explorer
 export const explorerSlugs: Record<number, string> = {
@@ -35,9 +33,7 @@ export const explorerSlugs: Record<number, string> = {
 };
 
 const wagmiNetworks = {
-  bellecour,
   arbitrumSepolia,
-  arbitrum,
 };
 
 export default wagmiNetworks;
